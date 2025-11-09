7af5d4b90a2d65d88e0523bb7568029ca6f529a7
DrPAHAN <mironenko.an7661@gmail.com>
2025-11-09 21:54:19 +0700

    Added update Streamlit.py

--- source/web/Streamlit.py
+++ source/web/Streamlit.py
@@ -1,24 +1,38 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import (
+    ResNet50,
+    preprocess_input,
+    decode_predictions,
+)
+
 
 # Заголовок приложения
 st.title("Классификация изображений с помощью ResNet50")
 
+
 # Описание
-st.write("Загрузите изображение, и модель ResNet50 предскажет, что на нем изображено.")
+st.write(
+    "Загрузите изображение, и модель ResNet50 предскажет, "
+    "что на нем изображено."
+)
+
 
 # Загрузка изображения
-uploaded_file = st.file_uploader("Выберите изображение...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader(
+    "Выберите изображение...", type=["jpg", "jpeg", "png"]
+)
+
 
 # Загрузка модели ResNet50
 @st.cache_resource
 def load_model():
     return ResNet50(weights='imagenet')
 
+
 model = load_model()
+
 
 # Функция для обработки изображения
 def preprocess_image(image):
@@ -31,6 +45,7 @@ def preprocess_image(image):
     # Предобработка изображения для ResNet50
     image_array = preprocess_input(image_array)
     return image_array
+
 
 # Обработка загруженного изображения
 if uploaded_file is not None:

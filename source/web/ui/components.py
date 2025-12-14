6603d6af472664ef86626640526445393809361c
vovan <ferrovovan@gmail.com>
2025-12-14 19:20:44 +0700

    Comment unused buttons

--- source/web/ui/components.py
+++ source/web/ui/components.py
@@ -32,14 +32,14 @@ def render_upload_panel():
 	infra_mode = st.checkbox("Enable infrared mode")
 
 	start_button = st.button("▶ Запустить обработку")
-	pause_button = st.button(
-		"⏸ Приостановить обработку",
-		key="pause_button",
-		on_click=pause_processing)
-	stop_button = st.button(
-		"⏹ Остановить обработку",
-		key="stop_button",
-		on_click=stop_processing)
+	# pause_button = st.button(
+	#	"⏸ Приостановить обработку",
+	#	key="pause_button",
+	#	on_click=pause_processing)
+	# stop_button = st.button(
+	#	"⏹ Остановить обработку",
+	#	key="stop_button",
+	#	on_click=stop_processing)
 
 	# Превращаем вход в temp-файл, если он загружен.
 	file_path = None
@@ -58,6 +58,6 @@ def render_upload_panel():
 		"skip_frame_rate": skip_frame_rate,
 		"infra_mode": infra_mode,
 		"start_button": start_button,
-		"pause_button": pause_button,
-		"stop_button": stop_button
+		# "pause_button": pause_button,
+		# "stop_button": stop_button
 	}

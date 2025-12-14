51ac28e9bf75b18fbadfa0310d1dbc9287cd0c64
vovan <ferrovovan@gmail.com>
2025-12-14 13:06:31 +0700

    Add tabylations and comment

--- source/data/simulate_ir.py
+++ source/data/simulate_ir.py
@@ -2,9 +2,13 @@ import cv2
 
 
 def rgb_to_ir(rgb_frame):
-    """
-    Простая заглушка для имитирования ИК-изображений
-    """
-    gray = cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2GRAY)
-    ir_sim = cv2.applyColorMap(gray, cv2.COLORMAP_INFERNO)
-    return ir_sim
+	"""
+	Простая заглушка для имитирования ИК-изображений
+	"""
+	# TODO: Правильное ИК-изображение  бит.
+	# ИК-кадр - это 1 канал 12/16
+	# А сейчас выдаётся цветное изображение - псевдо-RGB (3 канала, 8 бит)
+	# Это не правильно!
+	gray = cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2GRAY)
+	ir_sim = cv2.applyColorMap(gray, cv2.COLORMAP_INFERNO)
+	return ir_sim
